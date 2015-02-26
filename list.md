19941ab2451b65451aa01459c568fb9667cd47b8
etpinard <etienne.t.pinard@gmail.com>
2015-02-26 11:46:39 -0500

    first

---
+++ list.md
@@ -0,0 +1,17 @@
+### List of features
+
+##
+
+##
+
+- *periodic* projections (done with d3.geo.projection.rotate !!)
+
+- add maps to a <g class="basemaplayer"></g> node 
+  below <g class="maplayer"></g>
+
+
+##
+
+- Make `PROJECTION_TO_SCALE` Object (check datamaps)
+
+- Figure out [0, 360] vs. [-180, 180]
---
+++ raw/convert.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+vectors=(
+    "ne_110m_admin_0_countries"
+    "ne_110m_admin_1_states_provinces"
+    "ne_110m_coastline"
+    "ne_110m_land"
+    "ne_110m_ocean"
+)
+
+exts=(
+    "shp"
+    "shx"
+    "sbn"
+    "sbx"
+    "dbf"
+    "README.html"
+    "prj"
+    "VERSION.txt"
+)
+
+out="../world-110m.json"
+
+
+for i in ${vectors[@]}; do
+    unzip $i 
+done
+
+coastlines="ne_110m_coastline/ne_110m_coastline"
+
+topojson \
+    -o $out \
+    -- \
+    countries=${vectors[0]}.shp \
+    states_provinces=${vectors[1]}.shp \
+    coastline=${vectors[2]}.shp \
+    land=${vectors[3]}.shp \
+    ocean=${vectors[4]}.shp
+
+# requires also .shx
+
+for i in ${vectors[@]}; do
+    for j in ${exts[@]}; do
+        rm -f ${i}.${j}
+    done
+done
